fc9a6c3ee0d6b8ca6b87fda374ffdc12bb905b3e
Ewerton Cordeiro Miranda <sonardevplus@gmail.com>
2021-06-12 08:13:41 -0300

    Configuração inicial do GIT/GITHUB

---
+++ src/main/java/miranda/tester/tests/TesterAndTestsApplication.java
@@ -0,0 +1,13 @@
+package miranda.tester.tests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesterAndTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesterAndTestsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/miranda/tester/tests/TesterAndTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package miranda.tester.tests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesterAndTestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
